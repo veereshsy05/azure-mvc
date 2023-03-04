7bbba5c3afa8e49e4eaf520f1a4684c7d443e3de
veereshsy05 <126865802+veereshsy05@users.noreply.github.com>
2023-03-04 23:25:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -101,7 +101,7 @@ resource "random_pet" "rg_name" {
     location              = azurerm_resource_group.rg.location
     resource_group_name   = azurerm_resource_group.rg.name
     network_interface_ids = [azurerm_network_interface.my_terraform_nic.id]
-    size                  = "Standard_DS1_v2"
+    size                  = "Standard_D2s_v3"
   
     os_disk {
       name                 = "myOsDisk"
