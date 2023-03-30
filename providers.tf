d1ade067913f7a40cb9a1ea3703eb5460619a223
veereshsy05 <126865802+veereshsy05@users.noreply.github.com>
2023-03-30 14:35:16 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -19,4 +19,8 @@ terraform {
   
   provider "azurerm" {
     features {}
+  subscription_id   = "d26e7644-d1b3-4114-9b7c-88528234a238"
+  tenant_id         = "105b2061-b669-4b31-92ac-24d304d195dc"
+  client_id         = "12099e70-1e5a-48ca-8e9d-5392b01f61af"
+  client_secret     = "9j68Q~WykxqHMxJ-97RlXZMRnKUM9IOIVoVEWbLN"
   }
